85c43b5b2830a784d052d4cc334e2d4d55ef1df1
EXT Anthony Thornton <a.r.thornton@utwente.nl>
2025-06-24 11:53:08 +0200

    Update boundary.py

--- boundary.py
+++ boundary.py
@@ -1,5 +1,19 @@
 from base_types import *
 
+class some_boundary:
+     """This is abstract base boundary, at this level there is only an id"""
+    id: int = None
+    """ Unique identifier - maddatory"""
+
+
+class some_domain:
+    
+
+class
+
+    
+    
+
 
 
 
