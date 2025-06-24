b73347f33ceaa87139e9fbaa667867aa7691c0dd
EXT Anthony Thornton <a.r.thornton@utwente.nl>
2025-06-24 14:17:55 +0200

    Update boundary.py

--- boundary.py
+++ boundary.py
@@ -3,13 +3,20 @@ from base_types import *
 class some_boundary:
      """This is abstract base boundary, at this level there is only an id"""
     id: int = None
-    """ Unique identifier - maddatory"""
+    """ Unique identifier - mandatory"""
 
 
-class some_domain:
+class some_geometry:
+    """Most boundaries have a location they act in-on"""
+
+class some_geometry(line):
+    """Line class, this is defined by a vector and a distance of closes approch to the orgin""" 
+
+    normal: Vector3 = Vector3(0,0,0)
+    location: Int = None
+
+
     
-
-class
 
     
     
