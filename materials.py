fc29202bfab8deaa8b3c005d5ceb1fef30d8ef0b
adiprice <56220695+adiprice@users.noreply.github.com>
2025-11-25 15:44:00 +0100

    Update materials.py

--- materials.py
+++ materials.py
@@ -2,26 +2,22 @@ from base_types import *
 
 
 class material_general:
-    """These are shared material variables for all materials"""
+	"""These are shared material variables for all materials"""
 	
-    density: float = None
-    """**[mandatory]** material density - particle density [M L^{-3}]"""
-    
+	density: float = None
+	"""**[mandatory]** material density - particle density [M L^{-3}]"""
+	
 	friction_coefficient: float = None
-    """**[mandatory]** material friction coefficient [-]"""
-    
+	"""**[mandatory]** material friction coefficient [-]"""
+	
 	id: int = None
-    """**[mandatory]** material id [-]"""
-    
+	"""**[mandatory]** material id [-]"""
+	
 class material_spring_constants(material_general):
-    """This material describes linear-elastic behaviour with spring constants"""
+	"""This material describes linear-elastic behaviour with spring constants"""
 
-    normal_stiffness: float = None
-    """**[mandatory]** normal stiffness [F L^{-1}]"""
-    
+	normal_stiffness: float = None
+	"""**[mandatory]** normal stiffness [F L^{-1}]"""
+	
 	shear_stiffness: float = None
-    """**[mandatory]** shear stiffness [F L^{-1}]"""
-
-
-
-		
+	"""**[mandatory]** shear stiffness [F L^{-1}]"""
