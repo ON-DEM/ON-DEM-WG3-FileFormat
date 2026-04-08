8cf96f25b81a90fe50b3ff0d3397482dd78f25aa
adiprice <56220695+adiprice@users.noreply.github.com>
2026-04-08 11:53:27 +0200

    Update model.py
    changed to snake_case

--- model.py
+++ model.py
@@ -1,20 +1,20 @@
 from base_types import *
 
-class ElasticFrictional3D:
+class elastic_frictional_3D:
     """
     Cundall's contact model with linear elasticity and Coulombian friction \cite{Cundall1979}; $F_n=k_n u_n$, $\Delta F_s=k_s \dot u_s\, \Delta t$.
     """
     implementations: list = ['MercuryDPM', 'YADE']
     """codes implementing this model"""
 
-class LinearViscoelasticFrictional3D(ElasticFrictional3D):
+class linear_viscoelastic_frictional_3D(elastic_frictional_3D):
     """
     Elastic-frictional model with viscous damping
     """
     implementations: list = ['MercuryDPM', 'YADE']
     """codes implementing this model"""
 
-class ElasticFrictional6D(ElasticFrictional3D):
+class elastic_frictional_6D(elastic_frictional_3D):
     """
     Generalized elastic-frictional model with rolling and twisting.
     """
