284c4648508733a4e9c28b3818ef684dc74d3120
Bruno Chareyre <bruno.chareyre@grenoble-inp.fr>
2026-04-29 12:17:20 +0200

    Reorder attributes in intr class with comments

--- interaction.py
+++ interaction.py
@@ -3,8 +3,9 @@ from base_types import *
 
 class intr:
     """This is generic pair interaction and this is a long comment continuing here"""
-    id1: int = None
     model: str = None
+    """**[mandatory]** which model applies to this interaction"""
+    id1: int = None
     """**[mandatory]** id of body 1"""
     id2: int = None
     """**[mandatory]** id of body 2"""
