a150cb0cd307cdf5b27ef5c268abd313debc3377
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2026-04-30 16:53:06 +0200

    remove spaces in latex

--- materials.py
+++ materials.py
@@ -5,7 +5,7 @@ class material_general:
 	"""These are shared material variables for all materials"""
 	
 	density: float = None
-	"""**[mandatory]** material density - particle density, $\rho$, [$M L^{-3}$]"""
+	"""**[mandatory]** material density - particle density, $\rho$, [$ML^{-3}$]"""
 	
 	friction_coefficient: float = None
 	"""**[mandatory]** material friction coefficient, $\mu$, [$-$]"""
@@ -17,25 +17,25 @@ class material_spring_constants(material_general):
 	"""This material describes linear-elastic behaviour with spring constants"""
 
 	normal_stiffness: float = None
-	"""**[mandatory]** normal stiffness, $K_{n}$, [$F L^{-1}$]"""
+	"""**[mandatory]** normal stiffness, $K_{n}$, [$FL^{-1}$]"""
 	
 	shear_stiffness: float = None
-	"""**[mandatory]** shear stiffness, $K_{s}$, [$F L^{-1}$]"""
+	"""**[mandatory]** shear stiffness, $K_{s}$, [$FL^{-1}$]"""
 
 class material_spring_dashpot(material_spring_constants):
 	"""This material describes linear viscoelastic behaviour"""
 
 	normal_viscosity: float = None
-	"""**[mandatory]** normal damping coefficient, $c_{n}$, [$M T^{-1}$]"""
+	"""**[mandatory]** normal damping coefficient, $c_{n}$, [$MT^{-1}$]"""
 	
 	shear_viscosity: float = None
-	"""**[mandatory]** shear damping coefficient, $c_{s}$, [$M T^{-1}$]"""
+	"""**[mandatory]** shear damping coefficient, $c_{s}$, [$MT^{-1}$]"""
 
 class material_elastic_constants(material_general):
 	"""This material describes Hertzian-elastic behaviour with elasticity parameters"""
 
 	young: float = None
-	"""**[mandatory]** young modulus, $E$, [$F L^{-2}$]"""
+	"""**[mandatory]** young modulus, $E$, [$FL^{-2}$]"""
 	
 	poisson: float = None
 	"""**[mandatory]** poisson coefficient, $\nu$, [$-$]"""
