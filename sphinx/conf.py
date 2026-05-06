e8a7b71758d1c367e2f44cc36298c9df6a3c560f
bchareyre <bruno.chareyre@grenoble-inp.fr>
2026-05-06 18:35:47 +0200

    switch html/sphinx style

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -128,9 +128,19 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'agogo'
-html_theme = 'alabaster'
-#html_theme = 'nature'
+html_theme = 'agogo'
+# html_theme = 'alabaster'
+# html_theme = 'nature'
+# html_theme = 'sphinxdoc'
+# html_theme = 'classic'
+
+# html_theme = 'default'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {'stickysidebar': 'true', 'collapsiblesidebar': 'true', 'rightsidebar': 'false'}
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
