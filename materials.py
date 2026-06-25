f5020a1077da1438e8c159e7e37af0fa170ddc50
Thomas Weinhart <t.weinhart@utwente.nl>
2026-06-25 15:26:23 +0200

    Fixed naming suggestions and docstrings

--- materials.py
+++ materials.py
@@ -2,7 +2,7 @@ from base_types import *
 
 
 class material_general:
-	"""General properties shared by all materials"""
+	"""General properties shared by all materials."""
 
 	id: int = None
 	"""**[mandatory]** material id, $id$, [$-$]"""
@@ -11,8 +11,8 @@ class material_general:
 	"""**[mandatory]** particle density, $\\rho$, [$ML^{-3}$]"""
 
 
-class linear_elastic:
-	"""Linear-elastic behaviour"""
+class linear_elastic(material_general):
+	"""Material with linear-elastic behaviour."""
 
 	normal_stiffness: float = None
 	"""**[mandatory]** normal stiffness, $K_n$, [$FL^{-1}$]"""
@@ -21,8 +21,8 @@ class linear_elastic:
 	"""**[mandatory]** shear stiffness, $K_s$, [$FL^{-1}$]"""
 
 
-class hertz_elastic:
-	"""Hertzian-elastic behaviour"""
+class hertz_elastic(material_general):
+	"""Material with Hertzian-elastic behaviour."""
 
 	young_modulus: float = None
 	"""**[mandatory]** Young's modulus, $E$, [$FL^{-2}$]"""
@@ -32,26 +32,28 @@ class hertz_elastic:
 
 
 class linear_visco_elastic(linear_elastic):
-	"""Visco-elastic damping behaviour"""
+	"""Material with linear visco-elastic damping behaviour."""
 
 	normal_damping: float = None
 	"""**[mandatory]** normal damping coefficient, $c_n$, [$MT^{-1}$]"""
 
-class linear_visco_elastic_constant_COR(linear_elastic):
-	"""This describes visco-elastic behaviour with mas-independent coefficient of restitution"""
+
+class linear_visco_elastic_constant_cor(linear_elastic):
+	"""Material with linear visco-elastic damping behaviour and mass-independent coefficient of restitution."""
 
 	normal_damping_ratio: float = None
 	"""**[mandatory]** normal damping coefficient, $\\beta_n$  [$-$]"""
 
-class linear_visco_elastic_variable_COR(linear_elastic):
-	"""This material describes visco-elastic behaviour with variable coefficient of restitution"""
+
+class linear_visco_elastic_variable_cor(linear_elastic):
+	"""Material with linear visco-elastic damping behaviour and velocity-dependent coefficient of restitution."""
 
 	relaxation_time: float = None
 	"""**[mandatory]** relaxation time, $A$, [$T$]"""
 
 
-class frictional_3d:
-	"""Sliding friction behaviour"""
+class frictional_3D:
+	"""Adds sliding friction behaviour to any material."""
 
 	shear_friction: float = None
 	"""**[mandatory]** sliding-friction coefficient, $\\mu_s$, [$-$]"""
@@ -59,35 +61,38 @@ class frictional_3d:
 	shear_damping: float = None
 	"""**[mandatory]** shear damping coefficient, $c_s$, [$MT^{-1}$]"""
 
-class frictional_6d(frictional_3d):
-	"""Sliding, rolling and torsional friction behaviour"""
 
-	model: str = "frictional_6d"
-	"""friction model"""
+class frictional_6D(frictional_3D):
+	"""Adds sliding, rolling and torsional friction to any material."""
+
+	model: str = "frictional_6D"
+	"""Friction model."""
 
 	rolling_friction: float = None
 	"""**[mandatory]** rolling-friction coefficient, $\\mu_r$, [$-$]"""
 
 	rolling_damping: float = None
-	"""**[mandatory]** shear damping coefficient, $c_s$, [$MT^{-1}$]"""
+	"""**[mandatory]** rolling damping coefficient, $c_r$, [$MT^{-1}$]"""
 
 	torsion_friction: float = None
 	"""**[mandatory]** torsional-friction coefficient, $\\mu_t$, [$-$]"""
 
-	rolling_damping: float = None
-	"""**[mandatory]** shear damping coefficient, $c_s$, [$MT^{-1}$]"""
+	torsion_damping: float = None
+	"""**[mandatory]** torsional damping coefficient, $c_t$, [$MT^{-1}$]"""
 
-class material_liquid_bridge(material_general):
-	"""This material describes a liquid bridge model"""
 
-	surfaceTension: float = None
+class liquid_bridge:
+	"""Adds a liquid-bridge model to any material."""
+
+	surface_tension: float = None
 	"""**[mandatory]** surface tension of the liquid, $\\gamma$ [$F/L$]"""
 
-	contactAngle: float = None
+	contact_angle: float = None
 	"""**[mandatory]** contact angle between particle and liquid bridge surface, $\\Theta$ [$-$]"""
 
-class material_liquid_bridge_migration(material_liquid_bridge):
-	"""This material describes a liquid bridge migration model"""
+
+class liquid_bridge_migration(liquid_bridge):
+	"""Adds a liquid-bridge migration model to any material."""
 
 	liquid_bridge_volume_min: float = None
 	"""**[mandatory]** the minimum liquid volume needed to form a bridge, $V_\\mathrm{min}$ [$L^3$]"""
@@ -98,22 +103,33 @@ class material_liquid_bridge_migration(material_liquid_bridge):
 	distribution_coefficient: float = None
 	"""**[mandatory]** the fraction of the liquid that is to be distributed to the neighboring contacts of the particles after a liquid bridge rupture, $f$ [$-$]"""
 
-class material_liquid_bridge_static(material_liquid_bridge):
-	"This material describes a liquid bridge model, where every contact has the same liquid volume"
+
+class liquid_bridge_static(liquid_bridge):
+	"""Adds a liquid-bridge model in which every contact has the same liquid volume."""
 
 	liquid_bridge_volume: float = None
 	"""**[mandatory]** the liquid volume of a bridge, $V$ [$L^3$]"""
 
-class material_linear_elastic_frictional_3D(material_general, linear_elastic, frictional_3d):
-	"""General material with linear elastic frictional contact forces"""
-	pass
-class material_linear_visco_elastic_frictional_3d(material_general, linear_visco_elastic, frictional_3d):
-	"""General material with linear visco-elastic frictional_3d contact forces"""
-	pass
-class material_linear_elastic_frictional_6D(material_general, linear_visco_elastic, frictional_6d):
-	"""General material with linear elastic frictional_6d contact forces"""
+
+class linear_elastic_frictional_3D(linear_elastic, frictional_3D):
+	"""Material with linear-elastic behaviour and sliding friction."""
 	pass
 
-class material_liquid_bridge_linear_visco_elastic_frictional_3d(material_liquid_bridge, linear_visco_elastic, frictional_3d):
-	"""Material with liquid bridges and linear visco-elastic frictional contact forces"""
+
+class linear_visco_elastic_frictional_3D(linear_visco_elastic, frictional_3D):
+	"""Material with linear visco-elastic behaviour and sliding friction."""
+	pass
+
+
+class linear_elastic_frictional_6D(linear_elastic, frictional_6D):
+	"""Material with linear-elastic behaviour and sliding, rolling and torsional friction."""
+	pass
+
+
+class linear_visco_elastic_frictional_3D_liquid_bridge(
+	linear_visco_elastic,
+	frictional_3D,
+	liquid_bridge
+):
+	"""Material with linear visco-elastic behaviour, sliding friction and liquid-bridge forces."""
 	pass
