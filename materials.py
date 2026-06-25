27da3a8a6775f8ea939f8bc0130ca9d1f3f24da0
Thomas Weinhart <t.weinhart@utwente.nl>
2026-06-25 16:14:59 +0200

    Updated model.py and materials.py to allow composition of force models.
    materials now contain
    - force components: linear_elastic, hertz_elastic, linear_visco_elastic, frictional_3D, frictional_6D
    - materials: material_general, material_liquid_bridge, etc
    these components can be combined into a material model with forces by multiple inheritance, e.g.
    class material_linear_visco_elastic_frictional_3d(material_general, linear_visco_elastic, frictional_3d)
    this makes it easier to create the multiple force models we have implemented.

--- materials.py
+++ materials.py
@@ -2,121 +2,185 @@ from base_types import *
 
 
 class base_material:
-    """These are shared material variables for all materials"""
-    
-    density: float = None
-    """**[mandatory]** material density - particle density, $\rho$, [$ML^{-3}$]"""
-    
-    friction_coefficient: float = None
-    """**[mandatory]** material friction coefficient, $\\mu$, [$-$]"""
-    
+    """General properties shared by all materials."""
+
     id: int = None
     """**[mandatory]** material id, $id$, [$-$]"""
-    
-class spring_constants(base_material):
-    """This material describes linear-elastic behaviour with spring constants"""
 
-    normal_stiffness: float = None
-    """**[mandatory]** normal stiffness, $K_{n}$, [$FL^{-1}$]"""
-    
-    shear_stiffness: float = None
-    """**[mandatory]** shear stiffness, $K_{s}$, [$FL^{-1}$]"""
+    density: float = None
+    """**[mandatory]** particle density, $\\rho$, [$ML^{-3}$]"""
 
-class spring_dashpot(spring_constants):
-    """This material describes linear viscoelastic behaviour"""
 
-    normal_viscosity: float = None
-    """**[mandatory]** normal damping coefficient, $c_{n}$, [$MT^{-1}$]"""
-    
-    shear_viscosity: float = None
-    """**[mandatory]** shear damping coefficient, $c_{s}$, [$MT^{-1}$]"""
+class linear_elastic(base_material):
+	"""Material with linear-elastic behaviour."""
 
-class elastic_constants(base_material):
-    """This material describes Hertzian-elastic behaviour with elasticity parameters"""
+	normal_stiffness: float = None
+	"""**[mandatory]** normal stiffness, $K_n$, [$FL^{-1}$]"""
 
-    young: float = None
-    """**[mandatory]** young modulus, $E$, [$FL^{-2}$]"""
-    
-    poisson: float = None
-    """**[mandatory]** poisson coefficient, $\nu$, [$-$]"""
+	shear_stiffness: float = None
+	"""**[mandatory]** shear stiffness, $K_s$, [$FL^{-1}$]"""
 
-class visco_elastic_constant_COR(elastic_constants):
-    """This material describes visco-elastic behaviour with constant coefficient of restitution"""
 
-    normal_damping_ratio: float = None
-    """**[mandatory]** normal damping coefficient, $\\beta_n$  [$-$]"""
-    
-    shear_damping_ratio: float = None
-    """**[mandatory]** shear damping coefficient, $\\beta_s$ [$-$]"""
+class hertz_elastic(base_material):
+	"""Material with Hertzian-elastic behaviour."""
 
-class visco_elastic_variable_COR(elastic_constants):
-    """This material describes visco-elastic behaviour with variable coefficient of restitution"""
+	young_modulus: float = None
+	"""**[mandatory]** Young's modulus, $E$, [$FL^{-2}$]"""
 
-    relaxation_time: float = None
-    """**[mandatory]** relaxation time, $A$, [$T$]"""
+	poisson_ratio: float = None
+	"""**[mandatory]** Poisson's ratio, $\\nu$, [$-$]"""
 
-class base_thermal(elastic_constants):
-    """This material describes thermal behaviour"""
-    thermal_conduction: float = None
-    """**[mandatory]** thermal conduction of the material, $c_v$, [$E/(L*\theta)$]"""
+
+class linear_visco_elastic(linear_elastic):
+	"""Material with linear visco-elastic damping behaviour."""
+
+	normal_damping: float = None
+	"""**[mandatory]** normal damping coefficient, $c_n$, [$MT^{-1}$]"""
+
+
+class linear_visco_elastic_constant_cor(linear_elastic):
+	"""Material with linear visco-elastic damping behaviour and mass-independent coefficient of restitution."""
+
+	normal_damping_ratio: float = None
+	"""**[mandatory]** normal damping coefficient, $\\beta_n$  [$-$]"""
+
+
+class linear_visco_elastic_variable_cor(linear_elastic):
+	"""Material with linear visco-elastic damping behaviour and velocity-dependent coefficient of restitution."""
+
+	relaxation_time: float = None
+	"""**[mandatory]** relaxation time, $A$, [$T$]"""
+
+
+class frictional_3D:
+	"""Adds sliding friction behaviour to any material."""
+
+	shear_friction: float = None
+	"""**[mandatory]** sliding-friction coefficient, $\\mu_s$, [$-$]"""
+
+	shear_damping: float = None
+	"""**[mandatory]** shear damping coefficient, $c_s$, [$MT^{-1}$]"""
+
+
+class frictional_6D(frictional_3D):
+	"""Adds sliding, rolling and torsional friction to any material."""
+
+	model: str = "frictional_6D"
+	"""Friction model."""
+
+	rolling_friction: float = None
+	"""**[mandatory]** rolling-friction coefficient, $\\mu_r$, [$-$]"""
+
+	rolling_damping: float = None
+	"""**[mandatory]** rolling damping coefficient, $c_r$, [$MT^{-1}$]"""
+
+	torsion_friction: float = None
+	"""**[mandatory]** torsional-friction coefficient, $\\mu_t$, [$-$]"""
+
+	torsion_damping: float = None
+	"""**[mandatory]** torsional damping coefficient, $c_t$, [$MT^{-1}$]"""
+
+
+class base_thermal(linear_elastic):
+    """Material with linear-elastic and thermal behaviour."""
+
+    thermal_conductivity: float = None
+    """**[mandatory]** thermal conductivity, $k$, [$E/(LT\\theta)$]"""
+
     thermal_capacity: float = None
-    """**[mandatory]** thermal capacity of the material, $c_p$, [$E/\theta$]"""
-    initial_themperature: float = None
-    """**[mandatory]** initial temperature the material, $T_0$, [$\theta$]"""
-    min_temp: float = None
-    """**[optional]** min temperature limit of the material,  $T_{min}$, [$\theta$]"""
-    max_temp: float = None
-    """**[optional]** max temperature limit of the material,  $T_{max}$, [$\theta$]"""
+    """**[mandatory]** thermal capacity, $c_p$, [$E/\\theta$]"""
 
-class thermal_Blaze(base_thermal):
-    """This material describes thermal behaviour dependent on friction and cohesion"""
+    initial_temperature: float = None
+    """**[mandatory]** initial temperature, $T_0$, [$\\theta$]"""
+
+    min_temp: float = None
+    """*[optional]* minimum temperature, $T_\\mathrm{min}$, [$\\theta$]"""
+
+    max_temp: float = None
+    """*[optional]* maximum temperature, $T_\\mathrm{max}$, [$\\theta$]"""
+
+
+class thermal_blaze(base_thermal):
+    """Material with frictional heat generation and cohesion evolution."""
 
     friction_source: float = None
-    """**[mandatory]** friction value used for heat generation, $\mu_{\theta}$, [$-$]"""
+    """**[mandatory]** friction coefficient used for heat generation, $\\mu_\\theta$, [$-$]"""
+
     contact_area_effective: float = None
-    """**[mandatory]** \% of contact area, $A_{eff}$, [$-$]"""
+    """**[mandatory]** effective fraction of the contact area, $A_\\mathrm{eff}$, [$-$]"""
+
     cohesion_rate: float = None
-    """**[mandatory]** cohesion rate, $SE_{rate}$, [$E/(L^2*T)$]"""
+    """**[mandatory]** cohesion rate, $SE_\\mathrm{rate}$, [$E/(L^2T)$]"""
+
     cohesion_limit: float = None
-    """**[optional]** max surface energy value, $SE_{max}$, [$E/L^2$]"""
+    """*[optional]* maximum surface energy, $SE_\\mathrm{max}$, [$E/L^2$]"""
+
     thickness: float = None
-    """**[optional]** artificial thickness for geometry, $\delta\ d$, [$L$]"""
-    
-class basic_luding(spring_constants):
-    """This material describes linear-elastic behaviour with spring constants"""
+    """*[optional]* artificial geometrical thickness, $\\delta d$, [$L$]"""
+
+
+class basic_luding(linear_elastic):
+    """Material with Luding-type elasto-plastic adhesive behaviour."""
 
     normal_stiffness_plastic: float = None
-    """**[mandatory]** normal stiffness in plastic branch, $K_{1}$, [$FL^{-1}$]"""
+    """**[mandatory]** plastic loading stiffness, $K_1$, [$FL^{-1}$]"""
 
     normal_stiffness_unloading_reloading: float = None
-    """**[mandatory]** normal stiffness in unloading and reloading elastic branch, $K_{n}^{U}$, [$FL^{-1}$]"""
-    
+    """**[mandatory]** unloading and reloading stiffness, $K_n^U$, [$FL^{-1}$]"""
+
     normal_stiffness_tensile: float = None
-    """**[mandatory]** normal stiffness in tensile adhesive branch, $K_{adh}^{U}$, [$FL^{-1}$]"""
+    """**[mandatory]** tensile adhesive stiffness, $K_\\mathrm{adh}^U$, [$FL^{-1}$]"""
 
-class liquid_bridge(base_material):
-    """This material describes a liquid bridge model"""
 
-    surfaceTension_: float = None
-    """**[mandatory]** surface tension of the liquid, $\\gamma$ [$F/L$]"""
+class liquid_bridge:
+	"""Adds a liquid-bridge model to any material."""
 
-    contactAngle_: float = None
-    """**[mandatory]** contact angle between particle and liquid bridge surface, $\\Theta$ [$-$]"""
+	surface_tension: float = None
+	"""**[mandatory]** surface tension of the liquid, $\\gamma$ [$F/L$]"""
+
+	contact_angle: float = None
+	"""**[mandatory]** contact angle between particle and liquid bridge surface, $\\Theta$ [$-$]"""
+
 
 class liquid_bridge_migration(liquid_bridge):
-    """This material describes a liquid bridge migration model"""
+	"""Adds a liquid-bridge migration model to any material."""
 
-    liquid_bridge_volume_min: float = None
-    """**[mandatory]** the minimum liquid volume needed to form a bridge, $V_\\mathrm{min}$ [$L^3$]"""
+	liquid_bridge_volume_min: float = None
+	"""**[mandatory]** the minimum liquid volume needed to form a bridge, $V_\\mathrm{min}$ [$L^3$]"""
 
-    liquid_bridge_volume_max: float = None
-    """**[mandatory]** the maximum liquid volume, a liquid bridge gets during formation, $V_\\mathrm{max}$ [$L^3$]"""
+	liquid_bridge_volume_max: float = None
+	"""**[mandatory]** the maximum liquid volume, a liquid bridge gets during formation, $V_\\mathrm{max}$ [$L^3$]"""
 
-    distribution_coefficient: float = None
-    """**[mandatory]** the fraction of the liquid that is to be distributed to the neighboring contacts of the particles after a liquid bridge rupture, $f$ [$-$]"""
+	distribution_coefficient: float = None
+	"""**[mandatory]** the fraction of the liquid that is to be distributed to the neighboring contacts of the particles after a liquid bridge rupture, $f$ [$-$]"""
+
 
 class liquid_bridge_static(liquid_bridge):
-    "This material describes a liquid bridge model, where every contact has the same liquid volume"
+	"""Adds a liquid-bridge model in which every contact has the same liquid volume."""
 
-    liquid_bridge_volume: float = None
-    """**[mandatory]** the liquid volume of a bridge, $V$ [$L^3$]"""
+	liquid_bridge_volume: float = None
+	"""**[mandatory]** the liquid volume of a bridge, $V$ [$L^3$]"""
+
+
+class linear_elastic_frictional_3D(linear_elastic, frictional_3D):
+	"""Material with linear-elastic behaviour and sliding friction."""
+	pass
+
+
+class linear_visco_elastic_frictional_3D(linear_visco_elastic, frictional_3D):
+	"""Material with linear visco-elastic behaviour and sliding friction."""
+	pass
+
+
+class linear_elastic_frictional_6D(linear_elastic, frictional_6D):
+	"""Material with linear-elastic behaviour and sliding, rolling and torsional friction."""
+	pass
+
+
+class linear_visco_elastic_frictional_3D_liquid_bridge(
+	linear_visco_elastic,
+	frictional_3D,
+	liquid_bridge
+):
+	"""Material with linear visco-elastic behaviour, sliding friction and liquid-bridge forces."""
+	pass
